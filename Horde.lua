e5d691bd181936e3e14bf34e5416a326ab5d1350
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-04-25 10:28:20 +0100

    Commit what I've done so far (which isn't much...)

--- Horde.lua
+++ Horde.lua
@@ -1,4 +1,55 @@
-﻿
+
 TourGuide:RegisterGuide("Midsummer Fire Festival (80+)", nil, "Horde", function() return [[
+
+N Wall of Text |N|This guide assumes you are level 80, and/or are in a large group, as you will be visiting your enemies' home cities during this tour. Also, desecrating enemy fires will flag you for PvP, so be sure to make a quick getaway! Good luck, brave adventurer!|
+
+h Dalaran |N|If you are currently in Dalaran, make sure to set your Hearthstone there if you haven't already done so. You'll be coming back later.|
+R Thunder Bluff |N|Use the portal|
+
+R Bloodhoof Village
+T Honor the Flame (Part 1) |N|(51,60)| |Z|Mulgore| |QID|11852|
+
+F Dustwallow Marsh
+T Honor the Flame (Part 2) |N|(33,30)| |Z|Dustwallow Marsh| |QID|11847|
+T Desecrate this Fire! (Part 1) |N|(62,40)| |Z|Dustwallow Marsh| |QID|11744|
+
+F Thousand Needles
+T Honor the Flame (Part 3) |N|(41,52)| |Z|Thousand Needles| |QID|11861|
+
+F Tanaris
+T Honor the Flame (Part 4) |N|(49,27)| |Z|Tanaris| |QID|11838|
+T Desecrate this Fire! (Part 2) |N|(52,29)| |Z|Tanaris| |QID|11762|
+
+F Silithus
+T Honor the Flame (Part 5) |N|(46,44)| |Z|Silithus| |QID|11836|
+T Desecrate this Fire! (Part 3) |N|(57,34)| |Z|Silithus| |QID|11760|
+
+F The Barrens
+T Honor the Flame (Part 6) |N|(72,47)| |Z|The Barrens| |QID|11859|
+
+R Feralas |N|Go south, through the Thousand Needles|
+T Desecrate this Fire! (Part 4) |N|(28,44)| |Z|Feralas| |QID|11746|
+
+R Desolace
+T Honor the Flame (Part 7) |N|(26,76)| |Z|Desolace| |QID|11845|
+T Desecrate this Fire! (Part 5) |N|65,17)| |Z|Desolace| |QID|11741|
+
+R Stonetalon Mountains
+T Honor the Flame (Part 8) |N|(50,60)| |Z|Stonetalon Mountains| |QID|11856|
+
+F The Crossroads
+T Honor the Flame (Part 9) |N|(52,28)| |Z|The Barrens| |QID|11859|
+
+R Durotar
+T Honor the Flame (Part 10) |N|(52,47)| |Z|Durotar| |QID|11846|
+
+B Booty Bay |N|Run to Ratchet and catch a boat|
+T Honor the Flame (Part 11) |N|(32,75) |Z|Stranglethorn Vale| |QID|11837|
+T Desecrate this Fire! (Part 6) |N|(33,73)| |Z|Stranglethorn Vale| |QID|11761|
+
+F Stonard
+T Honor the Flame (Part 12) |N|(47,47)| |Z|Swamp of Sorrows| |QID|11857|
+
+
 
 ]] end)
