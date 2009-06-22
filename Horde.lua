5e1cabd381c7a8c43375a68d83a07e17d32b4137
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-06-22 14:15:10 +0100

    Updated Kalimdor co-ordinates and added a bonfire I missed.

--- Horde.lua
+++ Horde.lua
@@ -10,45 +10,46 @@ R Bloodhoof Village
 T Honor the Flame (Part 1) |N|(51,60)| |Z|Mulgore| |QID|11852|
 
 F Dustwallow Marsh
-T Honor the Flame (Part 2) |N|(33,30)| |Z|Dustwallow Marsh| |QID|11847|
-T Desecrate this Fire! (Part 1) |N|(62,40)| |Z|Dustwallow Marsh| |QID|11744|
+T Honor the Flame (Part 2) |N|(33.5,31)| |Z|Dustwallow Marsh| |QID|11847|
+T Desecrate this Fire! (Part 1) |N|(61.8,40.5)| |Z|Dustwallow Marsh| |QID|11744|
 
 F Thousand Needles
 T Honor the Flame (Part 3) |N|(41,52)| |Z|Thousand Needles| |QID|11861|
 
 F Tanaris
-T Honor the Flame (Part 4) |N|(49,27)| |Z|Tanaris| |QID|11838|
-T Desecrate this Fire! (Part 2) |N|(52,29)| |Z|Tanaris| |QID|11762|
+T Honor the Flame (Part 4) |N|(49.8,26.9)| |Z|Tanaris| |QID|11838|
+T Desecrate this Fire! (Part 2) |N|(52.8,29.4)| |Z|Tanaris| |QID|11762|
 
 F Silithus
-T Honor the Flame (Part 5) |N|(46,44)| |Z|Silithus| |QID|11836|
-T Desecrate this Fire! (Part 3) |N|(57,34)| |Z|Silithus| |QID|11760|
+T Honor the Flame (Part 5) |N|(46.4,44.3)| |Z|Silithus| |QID|11836|
+T Desecrate this Fire! (Part 3) |N|(57.5,35.4)| |Z|Silithus| |QID|11760|
 
 F The Barrens
 T Honor the Flame (Part 6) |N|(72,47)| |Z|The Barrens| |QID|11859|
 
 R Feralas |N|Go south, through the Thousand Needles|
-T Desecrate this Fire! (Part 4) |N|(28,44)| |Z|Feralas| |QID|11746|
+T Desecrate this Fire! (Part 4) |N|(28.3,43.8)| |Z|Feralas| |QID|11746|
+T Honor the Flame (Part 7) |N|(72.4,47.8)| |Z|Ferelas| |QID|11849|
 
 R Desolace
-T Honor the Flame (Part 7) |N|(26,76)| |Z|Desolace| |QID|11845|
-T Desecrate this Fire! (Part 5) |N|65,17)| |Z|Desolace| |QID|11741|
+T Honor the Flame (Part 8) |N|(26.1,76.8)| |Z|Desolace| |QID|11845|
+T Desecrate this Fire! (Part 5) |N|66.1,17.2)| |Z|Desolace| |QID|11741|
 
 R Stonetalon Mountains
-T Honor the Flame (Part 8) |N|(50,60)| |Z|Stonetalon Mountains| |QID|11856|
+T Honor the Flame (Part 9) |N|(50,60)| |Z|Stonetalon Mountains| |QID|11856|
 
 F The Crossroads
-T Honor the Flame (Part 9) |N|(52,28)| |Z|The Barrens| |QID|11859|
+T Honor the Flame (Part 10) |N|(52,28)| |Z|The Barrens| |QID|11859|
 
 R Durotar
-T Honor the Flame (Part 10) |N|(52,47)| |Z|Durotar| |QID|11846|
+T Honor the Flame (Part 11) |N|(52,47)| |Z|Durotar| |QID|11846|
 
 b Booty Bay |N|Run to Ratchet and catch a boat|
-T Honor the Flame (Part 11) |N|(32,75) |Z|Stranglethorn Vale| |QID|11837|
+T Honor the Flame (Part 12) |N|(32,75) |Z|Stranglethorn Vale| |QID|11837|
 T Desecrate this Fire! (Part 6) |N|(33,73)| |Z|Stranglethorn Vale| |QID|11761|
 
 F Stonard
-T Honor the Flame (Part 12) |N|(47,47)| |Z|Swamp of Sorrows| |QID|11857|
+T Honor the Flame (Part 13) |N|(47,47)| |Z|Swamp of Sorrows| |QID|11857|
 
 R Blasted Lands
 T Desecrate this Fire! (Part 7) |N|(58,17)| |Z|Blasted Lands| |QID|11737|
@@ -70,10 +71,10 @@ T Desecrate this Fire! (Part 11) |N|(24,59)| |Z|Redridge Mountains| |QID|11751|
 
 R Burning Steppes
 T Desecrate this Fire! (Part 12) |N|(80,62)| |Z|Burning Steppes| |QID|11739|
-T Honor the Flame (Part 13) |N|(62,29)| |Z|Burning Steppes| |QID|11844|
+T Honor the Flame (Part 14) |N|(62,29)| |Z|Burning Steppes| |QID|11844|
 
 F Kargath
-T Honor the Flame (Part 14) |N|(4,49)| |Z|Badlands| |QID|11842|
+T Honor the Flame (Part 15) |N|(4,49)| |Z|Badlands| |QID|11842|
 
 R Loch Modan
 T Desecrate this Fire! (Part 13) |N|(32,40)| |Z|Loch Modan| |QID|11749|
@@ -89,78 +90,78 @@ T Desecrate this Fire! (Part 15) |N|(13,47)| |Z|Wetlands| |QID|11757|
 
 R Arathi Highlands
 T Desecrate this Fire! (Part 16) |N|(50,44)| |Z|Arathi Highlands| |QID|11732|
-T Honor the Flame (Part 15) |N|(74,41)| |Z|Arathi Highlands| |QID|11840|
+T Honor the Flame (Part 16) |N|(74,41)| |Z|Arathi Highlands| |QID|11840|
 
 F Tarren Mill
-T Honor the Flame (Part 16) |N|(58,25)| |Z|Hillsbrad Foothils| |QID|11853|
+T Honor the Flame (Part 17) |N|(58,25)| |Z|Hillsbrad Foothils| |QID|11853|
 T Desecrate this Fire! (Part 17) |N|(50,46)| |Z|Hillsbrad Foothills| |QID|11748|
 
 F The Sepulcher |N|Fly from Tarren Mill|
-T Honor the Flame (Part 17) |N|(49,38)| |Z|Silverpine Forest| |QID|11584|
+T Honor the Flame (Part 18) |N|(49,38)| |Z|Silverpine Forest| |QID|11584|
 
 R Eversong Woods |N|Fly to the Undercity, use the Orb Of Translocation to reach Silvermoon, run out the front door.|
-T Honor the Flame (Part 18) |N|(46,50)| |Z|Eversong Woods| |QID|11848|
-T Honor the Flame (Part 19) |N|(46,26)| |Z|Ghostlands| |QID|11850|
+T Honor the Flame (Part 19) |N|(46,50)| |Z|Eversong Woods| |QID|11848|
+T Honor the Flame (Part 20) |N|(46,26)| |Z|Ghostlands| |QID|11850|
 
 F Revantusk Village
-T Honor the Flame (Part 20) |N|(76,74)| |Z|The Hinterlands| |QID|11860|
+T Honor the Flame (Part 21) |N|(76,74)| |Z|The Hinterlands| |QID|11860|
 T Desecrate this Fire! (Part 18) |N|(14,50)| |Z|The Hinterlands| |QID|11755|
 
 R Western Plaguelands
 T Desecrate this Fire! (Part 19) |N|(43,82)| |Z|Western Plaguelands| |QID|11756|
 
 R Tirisfal Glades
-T Honor the Flame (Part 21) |N|(57,52)| |Z|Tirisfal Glades| |QID|11862|
+T Honor the Flame (Part 22) |N|(57,52)| |Z|Tirisfal Glades| |QID|11862|
 
 F Everlook |N|Take the zepellin to Orgrimmar, then fly to Winterspring.|
-T Honor the Flame (Part 22) |N|(59,35)| |Z|Winterspring| |QID|11839|
-T Desecrate this Fire! (Part 23) |N|(62,35)| |Z|Winterspring| |QID|11763|
+T Honor the Flame (Part 23) |N|(59.9,35.7)| |Z|Winterspring| |QID|11839|
+T Desecrate this Fire! (Part 23) |N|(62.5,35.4)| |Z|Winterspring| |QID|11763|
 
 F Splintertree Post
-T Honor the Flame (Part 23) |N|(70,69)| |Z|Ashenvale| |QID|11841|
-T Desecrate this Fire! (Part 24) |N|(38,54)| |Z|Ashenvale| |QID|11734|
+T Honor the Flame (Part 24) |N|(70.2,69.4)| |Z|Ashenvale| |QID|11841|
+T Desecrate this Fire! (Part 24) |N|(37.7,54.7)| |Z|Ashenvale| |QID|11734|
 
 R Darkshore
-T Desecrate this Fire! (Part 25) |N|(37,46)| |Z|Darkshore| |QID|11740|
+T Desecrate this Fire! (Part 25) |N|(36.9,46.2)| |Z|Darkshore| |QID|11740|
 
 b Azuremyst Isle
 A Stealing the Exodar's Flame |N|Loot the bonfire for an item which starts this quest. (41,24)| |Z|The Exodar| |U|35569| |QID|11933|
-T Desecrate this Fire! (Part 26) |N|(44,53)| |Z|Azuremyst Isle| |QID|11735|
+T Desecrate this Fire! (Part 26) |N|(44.5,52.4)| |Z|Azuremyst Isle| |QID|11735|
 
 R Bloodmyst Isle
-T Desecrate this Fire! (Part 27) |N|(55,69)| |Z|Bloodmyst Isle| |QID|11738|
+T Desecrate this Fire! (Part 27) |N|(55.9,67.9)| |Z|Bloodmyst Isle| |QID|11738|
 
 b Rut'theran Village
 A Stealing Darnassus's Flame |N|Loot the bonfire for an item which starts this quest. (55,91)| |Z|Teldrassil| |U|23184| |QID|9332|
-T Desecrate this Fire! (Part 28) |N|(55,60)| |Z|Teldrassil| |QID|11753|
+T Desecrate this Fire! (Part 28) |N|(55.1,60.3)| |Z|Teldrassil| |QID|11753|
 
 R Shattrath City |N|Hearth to Dalaran, take the portal|
-T Honor the Flame (Part 24) |N|(52,43)| |Z|Terrokar Forest| |QID|11858|
+T Honor the Flame (Part 25) |N|(52,43)| |Z|Terrokar Forest| |QID|11858|
 T Desecrate this Fire! (Part 29) |N|(55,55)| |Z|Terrokar Forest| |QID|11754|
 
 R Shadowmoon Valley |N|If you don't already have a flying mount, pick one up while your in town.  It's a long run otherwise!|
 T Desecrate this Fire! (Part 30) |N|(40,55)| |Z|Shadowmoon Valley| |QID|11752|
-T Honor the Flame (Part 25) |N|(33,30)| |Z|Shadowmoon Valley| |QID|11855|
+T Honor the Flame (Part 26) |N|(33,30)| |Z|Shadowmoon Valley| |QID|11855|
 
 R Hellfire Peninsula
 T Desecrate this Fire! (Part 31) |N|(62,58)| |Z|Hellfire Peninsula| |QID|11747|
-T Honor the Flame (Part 26) |N|(55,40)| |Z|Hellfire Peninsula| |QID|11851|
+T Honor the Flame (Part 27) |N|(55,40)| |Z|Hellfire Peninsula| |QID|11851|
 
 R Zangarmarsh
 T Desecrate this Fire! (Part 32) |N|(69,52)| |Z|Zangarmarsh| |QID|11758|
-T Honor the Flame (Part 27) |N|(36,52)| |Z|Zangarmarsh| |QID|11863|
+T Honor the Flame (Part 28) |N|(36,52)| |Z|Zangarmarsh| |QID|11863|
 
 R Nagrand
-T Honor the Flame (Part 28) |N|(51,34)| |Z|Nagrand| |QID|11854|
+T Honor the Flame (Part 29) |N|(51,34)| |Z|Nagrand| |QID|11854|
 T Desecrate this Fire! (Part 33) |N|Nagrand| |QID|11750|
 
 R Blade's Edge Mountains
 T Desecrate this Fire! (Part 34) |N|(42,66)| |Z|Blade's Edge Mountains| |QID|11736|
-T Honor the Flame (Part 29) |N|(50,59)| |Z|Blade's Edge Mountains| |QID|11843|
+T Honor the Flame (Part 30) |N|(50,59)| |Z|Blade's Edge Mountains| |QID|11843|
 
 R Netherstorm
 T Desecrate this Fire! (Part 35) |N|(31,63)| |Z|Netherstorm| |QID|11759|
-T Honor the Flame (Part 30) |N|(32,68) |Z|Netherstorm| |QID|11835|
+T Honor the Flame (Part 31) |N|(32,68) |Z|Netherstorm| |QID|11835|
 
 T Stealing Stormwind's Flame |N|In any capital city except Dalaran.| |QID|9330|
 T Stealing Ironforge's Flame |QID|9331|
