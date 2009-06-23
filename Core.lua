30cd169ecaf130723a86c0fc5f891d873a88ea9e
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-06-23 10:01:06 +0100

    Bugfix. The status frame should now update properly when a flame is visited.

--- Core.lua
+++ Core.lua
@@ -5,10 +5,12 @@ F:SetScript("OnEvent", function(self, event, msg)
 	local action, quest = TourGuide:GetObjectiveInfo()
 
 	if action == "TURNIN" then
+		local string = quest:gsub(" %(Part %d%)", "") -- strip out part number
+
 		local _, _, text = msg:find("^(.-) completed%.$")
 
-		if type(text) == 'string' and quest == text then
-			TourGuide:CompleteQuest(text)
+		if type(text) == 'string' and string == text then
+			TourGuide:CompleteQuest(quest)
 			TourGuide:UpdateStatusFrame()
 		end
 	end
