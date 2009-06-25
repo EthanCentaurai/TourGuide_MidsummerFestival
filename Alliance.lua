43d0b3e93dfe155393a1161ec95ec1f856eedf5c
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-06-25 20:23:11 +0100

    Add in Aenora's submitted Alliance guide. Thanks!

--- Alliance.lua
+++ Alliance.lua
@@ -1,4 +1,293 @@
 
 TourGuide:RegisterGuide("Midsummer Fire Festival (80+)", nil, "Alliance", function() return [[
+N Notes |N|This guide is geared toward actually completing all the achievements necessary to become a Flame Warden. Check off and skip the extra quests if you just wish to use it as a guide to getting all the bonfires.|
+
+h Dalaran |N|If you are currently in Dalaran, make sure to set your Hearthstone there if you haven't already done so. You'll be coming back later.|
+
+R Darnassus |N|Use the portal|
+
+R Teldrassil |N|Exit Darnassus to the East and enter Teldrassil|
+T Honor the Flame (Part 1) |QID|11824| |N|Near Dolanaar (55.06,60.41)| |Z|Teldrassil| 
+
+R Darnassus |N|Return to Darnassus|
+F Auberdine |N|Fly to Auberdine in Darkshore|
+T Honor the Flame (Part 2) |QID|11811| |N|Right by the FP (36.96,46.17)| |Z|Darkshore| 
+
+b Azuremyst Isle
+R The Exodar
+
+A Incense for the Summer Scorchlings |N|Festival Loremaster in The Crystal Hall (41.61,25.23)| |Z|The Exodar| 
+
+A Torch Tossing |N|The Master Fire Eater (40.91,25.57)| |Z|The Exodar| 
+C Torch Tossing |N|Hit 8 braziers|
+T Torch Tossing |N|The Master Fire Eater (40.91,25.57)| |Z|The Exodar| 
+
+A Torch Catching |N|The Master Fire Eater (40.91,25.57)| |Z|The Exodar| 
+C Torch Catching |N|Catch 4 torches in a row|
+T Torch Catching |N|The Master Fire Eater (40.91,25.57)| |Z|The Exodar|  
+
+A More Torch Tossing |N|The Master Fire Eater (40.91,25.57)|
+C More Torch Tossing |N|Hit 8 braziers|
+T More Torch Tossing |N|The Master Fire Eater (40.91,25.57)| |Z|The Exodar| 
+
+A More Torch Catching |N|The Master Fire Eater (40.91,25.57)| |Z|The Exodar| 
+C More Torch Catching |N|Catch 4 torches in a row|
+T More Torch Catching |N|The Master Fire Eater (40.91,25.57)| |Z|The Exodar| 
+
+A Unusual Activity |N|The Earthen Ring Elder (43.27,26.24)|
+
+B [Juggling Torch] X20 |L|34599 20| |N|Buy 20 from the Midsummer Supplier (42.52,25.90). Get a few more if you are not confident in your ability to juggle. Save these for later.|
+
+R Azuremyst Isle |N|Head East from  The Exodar|
+T Honor the Flame (Part 3) |QID|11806| |N|Azuremyst Isle, on the road to Azure Watch (44.48,52.51)| |Z|Azuremyst Isle| 
+
+C Incense for the Summer Scorchlings |N|Find a Scorchling at or near the tent (44.41,52.48)|
+T Incense for the Summer Scorchlings
+
+R Bloodmyst Isle |N|Head North to Bloodmyst Isle|
+T Honor the Flame (Part 4) |QID|11809| |N|On the road to Blood Watch (55.82,67.89)| |Z|Bloodmyst Isle|
+
+b Darkshore |N|Return to the dock in Azuremyst Isle and catch the boat to Auberdine in Darkshore|
+
+F Everlook |N|Fly to Everlook in Winterspring|
+T Honor the Flame (Part 5) |QID|11834| |N|Near the Alliance FP (62.56,35.43)| |Z|Winterspring| 
+T Desecrate this Fire! (Part 1) |QID|11803| |N|Near the Horde FP (59.86,35.41)| |Z|Winterspring| 
+
+F Forest Song |N|Fly to Forest Song in Ashenvale.  If you don't have the FP, then go to Talrendis Point in Azshara.|
+T Desecrate this Fire! (Part 2)  |QID|11765| |N|On the road just past Splintertree Post (70.03,69.23)| |Z|Ashenvale| 
+T Honor the Flame (Part 6) |QID|11805| |N|On the road to Astranaar (37.78,54.73)| |Z|Ashenvale| 
+
+C Unusual Activity |N|Kill Twilight mobs near Blackfathom Depths until complete (16,19)| |Z|Ashenvale| 
+U Use the Totemic Beacon |U|35828|
+T Unusual Activity
+A An Innocent Disguise
+U Use Orb of the Crawler |U|35237| |N|Turn into a crab and wait for the naga to finish speaking (9.5,12.6)||Z|Ashenvale| 
+C An Innocent Disguise |QID|11891|
+U Use the Totemic Beacon |U|35828|
+T An Innocent Disguise
+A Inform the Elder
+
+F Stonetalon Peak |N|Head to Astranaar and fly to Stonetalon Peak in Stonetalon Mountains|
+T Desecrate this Fire! (Part 3) |QID|11780| |N|East of Sun Rock Retreat (50.59,60.36)| |Z|Stonetalon Mountains| 
+
+R Desolace |N|Head back North and cut through The Charred Vale. Go towards Nijel's Point in Desolace.|
+T Honor the Flame (Part 7) |QID|11812| |N|South of Nijel's Point (66.12,17.08)| |Z|Desolace| 
+T Desecrate this Fire! (Part 4) |QID|11769| |N|Near Shadowprey Village (26.11,77.26)| |Z|Desolace| 
+
+R Feathermoon Stronghold |N|Go South to Feralas and head to Feathermoon Stronghold|
+T Honor the Flame (Part 8) |QID|11817| |N|West of the FP (28.31,43.89)| |Z|Feralas| 
+
+F Thalanaar |N|Take the FP to Thalanaar|
+T Desecrate this Fire! (Part 5) |QID|11773| |N|West of Camp Mojache(72.42,47.55)| |Z|Feralas| 
+
+R Thousand Needles |N|Double back and head towards Freewind Post in Thousand Needles|
+T Desecrate this Fire! (Part 6) |QID|11785| |N|West of Freewind Post (42.45,52.69)| |Z|Thousand Needles| 
+
+R Thalanaar |N|Go back to the FP|
+F Cenarion Hold |N|Fly to Cenarion Hold in Silithus|
+T Honor the Flame (Part 9) |QID|11831| |N|Northeast of Cenarion Hold (57.48,35.24)| |Z|Silithus| 
+T Desecrate this Fire! (Part 7) |QID|11800| |N|Southwest of Cenarion Hold (46.40,44.75)| |Z|Silithus| 
+
+F Gadgetzan |N|Fly to Gadgetzan in Tanaris|
+T Honor the Flame (Part 10) |QID|11833| |N|East of Gadgetzan (52.76,29.37)| |Z|Tanaris| 
+T Desecrate this Fire! (Part 8) |QID|11802| |N|West of Gadgetzan (49.80,27.19)| |Z|Tanaris| 
+
+F Theramore Isle |N|Fly to Theramore Isle|
+T Honor the Flame (Part 11) |QID|11815| |N|On the road Northwest of Theramore (61.82,40.46)| |Z|Dustwallow Marsh| 
+T Desecrate this Fire! (Part 9) |QID|11771| |N|Southwest of Brackenwall (33.30,30.68)| |Z|Dustwallow Marsh| 
+
+R Mulgore |N|Cut through The Barrens and head towards Bloodhoof Village in Mulgore|
+T Desecrate this Fire! (Part 10) |QID|11777| |N|At the crossroads East of Bloodhoof Village (52.13,59.98)| |Z|Mulgore|
+
+R Thunder Bluff |N|Enter Thunder Bluff and Steal the Flame on Spirit Rise|
+A Stealinsg Thunder Bluff's Flame |QID|9325| |N|On the Spirit Rise (21.13,25.93). Loot the bonfire for an item which starts this quest.| |Z|Thunder Bluff|
+
+R The Barrens |N|Head back to The Barrens|
+T Desecrate this Fire! (Part 11) |QID|11783| |N|North of The Crossroads in The Barrens (52.13,27.91)| |Z|The Barrens| 
+
+R Durotar |N|Head East to Durotar|
+T Desecrate this Fire! (Part 12) |QID|11770| |N|Southwest of Razor Hill in Durotar (51.98,47.18)| |Z|Durotar| 
+
+R Orgrimmar |N|Enter Orgrimmar through the back door to Steal the Flame in the Valley of Wisdom|
+A Stealing Orgrimmar's Flame |QID|9324| |N|Valley of Wisdom (46.65,38.51). Loot the bonfire for an item which starts this quest.| |Z|Orgrimmar|
+
+R Ratchet |N|Return to The Barrens, head South to Ratchet and take the boat to Booty Bay|
+b Booty Bay
+T Honor the Flame (Part 12) |QID|11832| |N|On the beach Northeast of Booty Bay (33.89,73.53)| |Z|Stranglethorn Vale| 
+T Desecrate this Fire! (Part 13) |QID|11801| |N|Southwest down the beach from the Alliance fire (33.00,75.38) |Z|Stranglethorn Vale| 
+
+F Nethergarde Keep |N|Return to Booty Bay and take the FP to Nethergarde Keep in the Blasted Lands|
+T Honor the Flame (Part 13) |QID|11808| |N|To the West outside the keep (59.29,17.01)| |Z|Blasted Lands| 
+
+R Swamp of Sorrows |N|Ride North to Swamp of Sorrows|
+T Desecrate this Fire! (Part 14) |QID|11781| |N|In the swamp North of Stonard (46.76,46.46)| |Z|Swamp of Sorrows| 
+
+R Duskwood |N|Go West through Deadwind Pass to Duskwood|
+T Honor the Flame (Part 14) |QID|11814| |N|On the road South of Darkshire (73.70,54.63)| |Z|Duskwood| 
+
+F Redridge Mountains |N|Head to the FP and fly to Lakeshire in Redridge Mountains|
+T Honor the Flame (Part 15) |QID|11822| |N|Just West of the FP (25.24,58.96)| |Z|Redridge Mountains| 
+
+F Morgan's Vigil |N|Fly to Morgan's Vigil in Burning Steppes|
+T Honor the Flame (Part 16) |QID|11810| |N|Northwest of Morgan's Vigil (80.30,62.88)| |Z|Burning Steppes| 
+T Desecrate this Fire! (Part 15) |QID|11768| |N|Southwest of Flame Crest (62.22,29.02)| |Z|Burning Steppes| 
+
+R Morgan's Vigil |N|Return to Morgan's Vigil|
+F Sentinel Hill |N|Fly to Sentinel Hill in Westfall|
+T Honor the Flame (Part 17) |QID|11583| |N|Right next to the FP (55.93,53.47)| |Z|Westfall| 
+
+F Stormwind City |N|Take the FP to Stormwind|
+T Inform the Elder |QID|12012| |N|Earthen Ring Elder (49.32,72.28)| |Z|Stormwind City| 
+A Striking Back |N|This quest is different depending on your level. Complete it and turn it in at your convenience.|
+A Ahune, the Frost Lord |QID|11955|
+
+R Elwynn Forest |N|Exit Stormwind to Elwynn Forest|
+T Honor the Flame (Part 18) |QID|11816| |N|Northeast of the Inn in Goldshire (43.48,62.64)| |Z|Elwynn Forest|  
+
+R Stormwind City |N|Return to Stormwind City
+F Thorium Point |N|Fly to Thorium Point in Searing Gorge|
+R Badlands |N|Run East to the pass into the Badlands (69,54)|
+T Desecrate this Fire! (Part 16) |QID|11766| |N|Near Kargath in the Badlands (4.78,49.34)| |Z|Badlands| 
+
+R Loch Modan |N|Run East and North to Loch Modan|
+T Honor the Flame (Part 19) |QID|11820| |N|Northwest of Thelsamar on the road (32.52,40.98)| |Z|Loch Modan| 
+
+F Ironforge |N|Return to the FP and fly to Ironforge|
+R Dun Morogh |N|Exit Ironforge to Dun Morogh|
+T Honor the Flame (Part 20) |QID|11813| |N|On the road on the way to Kharanos (46.72,46.94)| |Z|Dun Morogh| 
+
+R Ironforge |N|Return to Ironforge|
+F Menethil Harbor |N|Fly to Menethil Harbor in the Wetlands|
+T Honor the Flame (Part 21) |QID|11828| |N|Northeast on the road outside Menethil Harbor (13.47, 47.09)| |Z|Wetlands| 
+
+F Refuge Pointe |N|Fly to Refuge Pointe in Arathi Highlands|
+T Honor the Flame (Part 22) |QID|11804| |N|Northeast of Refuge Pointe (50.02,44.79)| |Z|Arathi Highlands| 
+T Desecrate this Fire! (Part 17) |QID|11764| |N|South of Hammerfall (73.94,41.68)| |Z|Arathi Highlands| 
+
+R Refuge Pointe |N|Return to Refuge Pointe|
+F Southshore |N|Take the FP to Southshore|
+T Honor the Flame (Part 23) |QID|11819| |N|North of Southshore (50.47,47.60)| |Z|Hillsbrad Foothills| 
+T Desecrate this Fire! (Part 18) |QID|11776| |N|Southwest of Tarren Mill (58.38,25.21)| |Z|Hillsbrad Foothills| 
+
+R Silverpine Forest |N|Head West to Silverpine Forest|
+T Desecrate this Fire! (Part 19) |QID|11580| |N|East of The Sepulcher (49.64,38.64)| |Z|Silverpine Forest| 
+
+R Tirisfal Glades |N|Go North to Tirisfal Glades|
+T Desecrate this Fire! (Part 20) |QID|11786| |N|West of Brill (57.02,51.85)| |Z|Tirisfal Glades| 
+
+R Undercity |N|Enter the Undercity and steal the Flame in the Ruins of Lordaeron|
+A Stealing the Undercity's Flame |QID|9326| |N|Left side of entrance courtyard (67.93,8.34). Loot the bonfire for an item which starts this quest.| |Z|The Undercity|
+
+R Western Plaguelands |N| Head East to the Western Plaguelands|
+T Honor the Flame (Part 24) |QID|11827| |N|Near Chillwind Camp (43.48,82.32)| |Z|Western Plaguelands| 
+
+F Aerie Peak |N|Fly to Aerie Peak in the Hinterlands|
+T Honor the Flame (Part 25) |QID|11826| |N|South of Aerie Peak (14.35,50.11)| |Z|The Hinterlands| 
+T Desecrate this Fire! (Part 21) |QID|11784| |N|North of Revantusk Village (76.65,74.49)| |Z|The Hinterlands|
+
+R Aerie Peak |N|Return to Aerie Peak|
+F Ghostlands |N|Fly to Zul'Aman in the Ghostlands|
+T Desecrate this Fire! (Part 22) |QID|11774| |N|North of Tranquillien in Ghostlands (47.02,25.98)| |Z|Ghostlands| 
+T Desecrate this Fire! (Part 23) |QID|11772| |N|South of Ruins of Silvermoon in Eversong Woods (46.36,50.39)| |Z|Eversong Woods| 
+
+R Silvermoon City |N|Enter Silvermoon City and steal the Flame from the Court of the Sun|
+A Stealing Silvermoon's Flame |QID|11935||N|Court of the Sun (69.57,43.20). Loot the bonfire for an item which starts this quest.| |Z|Silvermoon City|
+
+
+H Dalaran
+C Torch Juggler |N|To do this, use the Juggling Torches that you purchased earlier. Find a quiet corner in Dalaran where you have open space and have the least amount of lag. Tips: Bind your Torches to a hotkey (1 for this example). Press 1 to get the targeting circle and aim directly beneath yourself. Throwing them at yourself this way will allow you to catch them as fast as you can throw them. There is no global cooldown on the torches so you can spam away until you complete this achievement.|
+
+R Shattrath City |N|Use the portal|
+F Telaar |N|Fly to Telaar in Nagrand|
+T Honor the Flame (Part 26) |N|Northwest of Telaar (49.63,69.44)| |Z|Nagrand| 
+T Desecrate this Fire! (Part 24) |QID|11778| |N|West of Garadar (51.06,34.11)| |Z|Nagrand| 
+
+R Zangarmarsh |N|Go North to Zangarmarsh|
+T Honor the Flame (Part 27) |QID|11829| |N|South of Telredor (68.79,51.93)| |Z|Zangarmarsh| 
+
+R Slave Pens
+T Ahune, the Frost Lord |QID|11955|
+A Ahune is Here! |QID|11696|
+T Ahune is Here! |QID|11696|
+
+A Summon Ahune |QID|11691| |N|Tatics: Everyone in your party can summon Ahune once per day. So you will be able to fight him 5 times if you wish. Ahune is a two-phase fight. Ahune does not need to be tanked. During the first phase, have your tank pick up the largest add and have most of your DPS focus on it. One DPS should be designated to picking up and killing the other little adds. Mainly to keep them off the healer. Phase two will begin when you get a raid warning that says something like 'Ahune has retreated'. He will change forms and become vulnerable to damage so all DPS needs to focus on him until phase 1 begins again. Note that Ahune will drop different loot depending on if the instance is normal or heroic.|
+
+A Shards of Ahune |QID|11972|
+T Shards of Ahune |QID|11972|
+
+R Zangarmarsh
+T Desecrate this Fire! (Part 25) |QID|11787| |N|East of Zabra'jin (35.61,51.76)| |Z|Zangarmarsh| 
+
+R Orebor Harborage |N|Head North to Orebor Harborage|
+F Sylvanaar |N|Fly to Sylvanaar in Blade's Edge Mountains|
+T Honor the Flame (Part 28) |QID|11807| |N|East of Sylvanaar in Blade's Edge Mountains (41.57,65.89)| |Z|Blade's Edge Mountains| 
+T Desecrate this Fire! (Part 26) |QID|11767| |N|Southwest of Thunderlord Stronghold (49.97,59.01)| |Z|Blade's Edge Mountains| 
+
+R Toshley's Station
+F Area 52 |N|Fly to Area 52 in Netherstorm|
+T Honor the Flame (Part 29) |QID|11830| |N|Just outside Area 52 (31.21,62.63)| |Z|Netherstorm| 
+T Desecrate this Fire! (Part 27) |QID|11799| |N|Just outside Area 52 (32.29,68.18) |Z|Netherstorm| 
+
+R Hellfire Peninsula |N|Fastest to just fly South on your mount|
+T Desecrate this Fire! (Part 28) |QID|11775| |N|Southeast of Thrallmar (57.15,41.81)| |Z|Hellfire Peninsula| 
+T Honor the Flame (Part 30) |QID|11818| |N|Northeast of Honor Hold (62.17,58.31)| |Z|Hellfire Peninsula| 
+
+R Shadowmoon Valley |N|Again, fastest to just fly South on your mount|
+T Desecrate this Fire! (Part 29) |QID|11779| |N|East of Shadowmoon Village (33.47,30.31)| |Z|Shadowmoon Valley| 
+T Honor the Flame (Part 31) |QID|11823| |N|Northeast of Wildhammer Stronghold (39.65,54.64)| |Z|Shadowmoon Valley| 
+
+F Allerian Stronghold |N|Fly to Allerian Stronghold in Terokkar Forest
+T Honor the Flame (Part 32) |QID|11825| |N|West of Allerian Stronghold (54.05,55.50)| |Z|Terokkar Forest| 
+T Desecrate this Fire! (Part 30) |QID|11782| |N|East of Stonebreaker Hold (51.93,43.24)| |Z|Terokkar Forest| 
+
+R Shattrath City |N|Once in Shatt, take a portal to any major city|
+
+T Stealing Silvermoon's Flame |QID|11935|
+T Stealing the Undercity's Flame |QID|9326|
+T Stealing Orgrimmar's Flame |QID|9324|
+T Stealing Thunder Bluff's Flame |QID|9325|
+T A Thief's Reward |QID|9365|
+
+B [Mantle of the Fire Festival] |L|23324|
+B [Vestment of Summer] |L|34685|
+B [Sandals of Summer] |L|34683|
+
+N Get dressed and dance |N|Equip your recently acquired summer garb and use a Ribbon Pole to dance for at least 60 seconds. Congrats! You're now a Flame Warden!|
+
+N Keep going? |N|Need more blossoms and cash?  Hit up the new Northrend bonfires. this guide is geared primarily towards using flight paths, flying it yourself is faster of course.|
+
+H Dalaran |N|Hearth to Dalaran|
+
+F Windrunner's Overlook |N|Fly to Windrunner's Overlook in Crystalsong Forest|
+T Honor the Flame (Part 33) |N|Northeast of Windrunner's Overlook (78.17,74.95) |Z|Crystalsong Forest|
+T Desecrate this Fire! (Part 31) |N|Southeast of Sunreaver's Command (80.34,52.87) |Z|Crystalsong Forest|
+
+T Honor the Flame (Part 34) |N|Southeast of K3 (41.44,86.69) |Z|The Storm Peaks|
+T Desecrate this Fire! (Part 32) |N|Northwest of K3 (40.41,85.61) |Z|The Storm Peaks|
+
+F The Argent Stand |N|Take the FP to The Argent Stand in Zul'Drak|
+T Honor the Flame (Part 35) |N|Northwest of The Argent Stand (40.38,61.30) |Z|Zul'Drak|
+T Desecrate this Fire! (Part 33) |N|Southeast of The Argent Stand (43.28,71.25) |Z|Zul'Drak|
+
+F Amberpine Lodge |N|Take the FP to Amberpine Lodge in Grizzly Hills|
+T Honor the Flame (Part 36) |N|East of Amberpine Lodge (33.90,60.45) |Z|Grizzly Hills|
+T Desecrate this Fire! (Part 34) |N|Northwest of Conquest Hold (19.08,61.42) |Z|Grizzly Hills|
+
+T Desecrate this Fire! (Part 35) |N|South of Camp Winterhoof in Howling Fjord (48.45,13.34) |Z|Howling Fjord|
+T Honor the Flame (Part 37) |N|West of Fort Wildervar (57.80,16.12) |Z|Howling Fjord|
+
+F Wintergarde Keep |N|Take the FP to Wintergarde Keep in Dragonblight|
+T Honor the Flame (Part 38) |N|Northwest of Wintergarde Keep (75.29,43.80) |Z|Dragonblight|
+T Desecrate this Fire! (Part 36) |N|Southeast of Agmar's Hammer (38.51,48.25) |Z|Dragonblight|
+
+R Stars' Rest |N|Go to Star's Rest to take the flyer, or just head to Borean Tundra|
+F Fizzcrank Airstrip |N|Fly to Fizzcrank Airstrip in Borean Tundra|
+T Honor the Flame (Part 39) |N|West of Fizzcrank Airstrip (55.10,19.95) |Z|Borean Tundra|
+T Desecrate this Fire! (Part 37) |N|Southeast of Bor'gorok Outpost (51.06,11.86) |Z|Borean Tundra|
+
+T Honor the Flame (Part 40) |N|At River's Heart in Sholazar Basin (48.09,66.37) |Z|Sholazar Basin|
+T Desecrate this Fire! (Part 38) |N|At River's Heart in Sholazar Basin (47.33,61.57) |Z|Sholazar Basin|
+
+N Done |N|That's all for 2009.  Hope you had fun.|
 
 ]] end)
